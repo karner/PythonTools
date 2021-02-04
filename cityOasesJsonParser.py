3d0a10766eeba44fbffdf8a43706d3fe2f0998d1
Mathias KARNER <karner@iiasa.ac.at>
2021-02-04 13:26:29 +0100

    Kinetica

--- cityOasesJsonParser.py
+++ cityOasesJsonParser.py
@@ -6,7 +6,7 @@ def _byteify(data, ignore_dicts = False):
         return data.encode('utf-8')
     # if this is a list of values, return list of byteified values
     if isinstance(data, list):
-        return [ _byteify(item, ignore_dicts=True) for item in data ]
+        return [_byteify(item, ignore_dicts=True) for item in data]
     # if this is a dictionary, return dictionary of byteified keys and values
     # but only if we haven't already byteified it
     if isinstance(data, dict) and not ignore_dicts:
--- tileDownloader.py
+++ tileDownloader.py
@@ -1,5 +1,17 @@
 import Tkinter as tk
 
+class Node:
+    def __init__(self):
+
+
+class Rectangle:
+    def __init__(self):
+        self.minX =
+        self.minY =
+        self.maxX =
+        self.maxY =
+
+
 class Application:
     def __init__(self, master):
         frame = tk.Frame(master)
@@ -12,6 +24,9 @@ class Application:
         self.hi_there = tk.Button(
             frame, text="Hello", command=self.say_hi)
         self.hi_there.pack(side=tk.LEFT)
+
+        self.txtMinLat = tk.Entry(frame, bd=5)
+        self.txtMinLat.pack(side=tk.LEFT)
 
     def say_hi(self):
         print "hi there, everyone!"
